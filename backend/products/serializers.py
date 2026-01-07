4156a9048736bc0e50d31e1f13182cddf3f583d8
DharaneeDharan <dharan@machers.tech>
2026-01-07 18:06:58 +0530

    serializer how it work in DRF backend side

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 from .models import product
 
 class ProductSerializer(serializers.ModelSerializer):
+    my_discount = serializers.SerializerMethodField(method_name='get_my_discount')
+    
     class Meta:
         model = product
-        fields = ['title','description','price','get_discount','sale_price']
+        # fields = ['title','description','price','get_discount','sale_price']
+        fields = ['title','description','price','my_discount','sale_price']
+
+    def get_my_discount(self, obj):
+        return obj.get_discount()
+        
         
