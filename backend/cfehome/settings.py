3b77f6ad198b4dd3efff908ddd7013043fa2e538
DharaneeDharan <dharan@machers.tech>
2026-01-12 17:58:12 +0530

    cors include

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
+    'corsheaders',
+    
     'article',
     'api',
     'products',
@@ -51,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -59,6 +62,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'cfehome.urls'
+CORS_URLS_REGEX = r'^/api/.*$'
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8111",
+    "https://localhost:8111"
+]
 
 TEMPLATES = [
     {
